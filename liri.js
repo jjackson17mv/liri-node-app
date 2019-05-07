c1478cbc6d7a87fc12b228a29708abec15cd7ef3
jax <jjackson17mv@gmail.com>
2019-05-07 01:06:56 -0700

    movies and concerts working

--- liri.js
+++ liri.js
@@ -1,14 +1,13 @@
 require("dotenv").config();
 
-var keys = require("./keys.js");
-var spotify = require('node-spotify-api');
+var keys = require('./keys.js');
+var Spotify = require('node-spotify-api');
 
 var spotify = new Spotify(keys.spotify);
-
-var moment = require('moment'); 
+var moment = require('moment');
 moment().format();
 
-var axios = require('axios'); 
+var axios = require('axios');
 
 var fs = require('fs'); // used to read the random.txt file required functions.
 var command = process.argv[2]; //send commands to switch arguments.
@@ -29,22 +28,103 @@ switch (command) {
         break;
 };
 
+var divider = "\n---------------------------------------------------------------------\n";
+
+//--------------------------------------CONCERT THIS/BANDS IN TOWN-------------------------------
+
 function concertThis(value) {
-    axios.get("https://rest.bandsintown.com/artists/" + value + "/events?app_id=trilogy")
+    axios.get("https://rest.bandsintown.com/artists/" + value + "/events?app_id=609dd5528abc9aa7755ea693f5e8fde9")
+        .then(function (response) {
+            console.log(response.data)
+
+            for (var i = 0; i < response.data.length; i++) {
+
+                var dateTime = response.data[i].dateTime;
+                var dateArr = dateTime.split("");
+
+                var concertResults =
+
+
+
+                    "\nVenue Name:" + response.data[1].venue.name +
+                    "\nVenue location:" + response.data[1].venue.location +
+                    "\nDate of Concert:" + moment(dataArr[0], "MM-DD-YYYY")
+                console.log(concertResults)
+
+            }
+        })
+
+        .catch(function (error) {
+            console.log(error);
+        });
+
+}
+//------------------------------------SPOTIFY-------------------
+function spotifySong(value) {
+    if (value) {
+        value = "I Want it That Way"
+    }
+    spotify
+        .search({ type: 'track', query: value })
+        .then(function (response) {
+            for (var i = 0; i < 5; i++) {
+                var spotifyResults =
+
+                    "\nArtist(s): " + response.tracks.items[i].artists[0].name +
+                    "\nSong Name: " + response.tracks.items[i].name +
+                    "\nAlbum Name: " + response.tracks.items[i].album.name +
+                    "\nPreview Link: " + response.tracks.items[i].preview_url;
+                console.log(spotifyResults);
+            }
+
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+}
+    
+
+//---------------------------MOVIE THIS-----------------------
+function movieThis(value) {
+    if (value) {
+        value = "mr nobody";
+
+    }
+}
+
+function movieThis(value) {
+    if(!value){
+        value = "mr nobody";
+    }
+    axios.get("https://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=trilogy")
     .then(function(response) {
-        console.log(response.data)
+            var movieResults = 
+                "--------------------------------------------------------------------" +
+                    "\nMovie Title: " + response.data.Title + 
+                    "\nYear of Release: " + response.data.Year +
+                    "\nIMDB Rating: " + response.data.imdbRating +
+                    "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value +
+                    "\nCountry Produced: " + response.data.Country +
+                    "\nLanguage: " + response.data.Language +
+                    "\nPlot: " + response.data.Plot +
+                    "\nActors/Actresses: " + response.data.Actors;
+            console.log(movieResults);
 
-        for(var i = 0; i < response.data.length; i++) {
-            
-            var dateTime = response.data[i].dateTime;
-            var dateArr = dateTime.split("");
+    })
 
-            var concertResults = 
-
-            "\nVenue Name:" + response.data[1].venue.name + 
-            "\nVenue location:" + response.data[1].venue.location +
-            "\nDate of Concert:"
-            
-        }
+    .catch(function (error) {
+        console.log(error);
     })
 }
+//---------------------------DO THIS-------------------
+function doThis(value) {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        var dataArr = data.split(',');
+        spotifySong(dataArr[0], dataArr[1]);
+    })
+}
+    
